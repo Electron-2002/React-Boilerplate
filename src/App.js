18b2c5d1a8409f1fe56b1cbb5cd4d7f9128298ae
Satej Bidvai <satej.bidvai@gmail.com>
2020-08-16 21:48:05 +0530

    ESLint and Pettier setup completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-	return <h2>Hello There!</h2>;
-}
+const App = () => {
+    return <h2>Hello There!</h2>;
+};
 
 export default App;
