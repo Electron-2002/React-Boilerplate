ee843cb58d987d6ef7800fb2ca5a8771b46d1e17
Satej Bidvai <satej.bidvai@gmail.com>
2020-08-17 00:52:14 +0530

    Code cleanup (Minor)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
-    return <Switch>
-      <Route to='/' exact component={() => <h1>Home Page</h1>}/>
-    </Switch>;
+    return (
+        <Switch>
+            <Route to="/" exact component={() => <h1>Home Page</h1>} />
+        </Switch>
+    );
 };
 
 export default App;
